a17ee06a0d6158331950d20bedc5bca89d7d4a9d
mciere1982 <37937144+mciere1982@users.noreply.github.com>
2020-06-29 20:53:59 +0200

    Test Commit (Klikken ipv Klik)

--- src/Vb0203.java
+++ src/Vb0203.java
@@ -21,7 +21,7 @@ class Paneel extends JPanel
 
     public Paneel()
     {
-        knop = new JButton("Klik");
+        knop = new JButton("Klikken");
         KnopHandler kh = new KnopHandler();
         knop.addActionListener(kh);
         tekstvak = new JTextField(10);
