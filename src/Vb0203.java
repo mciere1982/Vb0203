a4ed932822f64662a2bf83a64a2722096c3c5017
mciere1982 <37937144+mciere1982@users.noreply.github.com>
2020-06-29 16:20:49 +0200

    Initial commit

---
+++ src/Vb0203.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Vb0203 extends JFrame
+{
+    public static void main (String args[])
+    {
+        JFrame frame = new Vb0203();
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Voorbeeld 0203");
+        JPanel paneel = new Paneel();
+        frame.setContentPane(paneel);
+        frame.setVisible(true);
+    }
+}
+class Paneel extends JPanel
+{
+    private JButton knop;
+    private JTextField tekstvak;
+
+    public Paneel()
+    {
+        knop = new JButton("Klik");
+        KnopHandler kh = new KnopHandler();
+        knop.addActionListener(kh);
+        tekstvak = new JTextField(10);
+        add(knop);
+        add(tekstvak);
+    }
+    class KnopHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            tekstvak.setText("Je hebt geklikt!");
+        }
+    }
+}
